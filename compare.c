dea3f73d7ea9a36507694fe4268759489a46db75
Pradhyum Krishnan <pradhyum.krishnan@rutgers.edu>
2021-04-14 18:48:45 -0400

    added insertWFD method
    Co-authored-by: Gaurav Patel <GauravP2001@users.noreply.github.com>

--- compare.c
+++ compare.c
@@ -15,9 +15,16 @@ struct strbuff_t {
 
 struct llnode {
     int occurence; // number of times this word occurs in the doc
-//   int frequency; // number of times this word occurs in the doc / total number of words
+    double frequency; // number of times this word occurs in the doc / total number of words
     char* word;
     struct llnode* next;
+};
+
+struct fileRepository {
+    char* fileName;
+    int totalNodes;
+    struct llnode* list;
+    struct fileRepository* nextFile;
 };
 
 
@@ -31,12 +38,32 @@ void printList(struct llnode* list) {
     struct llnode* pointer = list;
 
     while (pointer != NULL) {
-        printf("%s | %d  -->  ", pointer->word, pointer->occurence);
+        printf("%s | %d | %f -->  ", pointer->word, pointer->occurence, pointer->frequency);
         pointer = pointer->next;
     }
 
     printf("\n");
 }
+
+void printWFDList(struct fileRepository* list) {
+    printf("\nWFD LIST\n");
+    struct fileRepository* file = list;
+
+    while (file != NULL) {
+        printf("File Name: %s\n", file->fileName);
+        struct llnode* word = file->list;
+        while (word != NULL) {        
+            printf("%s | %d | %f -->  ", word->word, word->occurence, word->frequency);
+            word = word->next;
+        }
+        printf("\n");
+
+        file = file->nextFile;
+    }
+
+    printf("\n");
+}
+
 
 void sb_destroy(char* L) {
     free(L);
@@ -114,6 +141,62 @@ struct llnode* insertNode(struct llnode* list, struct strbuff_t* sb) {
     return list;
 }
 
+void updateFrequencies(struct llnode** list) {
+    if (*list == NULL){
+        return;
+    }
+
+    // go through whole list and increase counter to get the number of nodes
+    // go through whole list again and just make freq = occ / total nodes 
+    int total_num = 0;
+    struct llnode *cur = *list;
+    while (cur != NULL) {
+        total_num += cur->occurence;
+        cur = cur->next;
+    }
+
+    cur = *list;
+
+    while (cur != NULL) {
+        cur->frequency = (double) cur->occurence/total_num;
+        cur = cur->next;
+    }
+
+    return;
+
+}
+
+struct fileRepository* insertWFD(struct fileRepository* fileList, struct llnode* wordList, char* fName) {
+
+    if (fileList == NULL) { // if there are no WFD structs yet
+        fileList = malloc(sizeof(struct fileRepository));
+        fileList->fileName = (char*) malloc(sizeof(char) * (strlen(fName) + 1));
+        strcpy(fileList->fileName, fName);
+        fileList->totalNodes = 0;
+        fileList->nextFile = NULL;
+        fileList->list = wordList;
+        return fileList;
+    }
+
+    struct fileRepository* curr = fileList;
+
+    while (curr->nextFile != NULL) {    // go to the last node
+        curr = curr->nextFile;
+    }
+
+    struct fileRepository* temp = malloc(sizeof(struct fileRepository));
+        
+    temp->totalNodes = 0;
+    temp->fileName = malloc(sizeof(char) * (strlen(fName) + 1));
+    strcpy(temp->fileName, fName);
+    temp->nextFile = NULL;
+    temp->list = wordList;
+
+    curr->nextFile = temp;
+
+    return fileList;
+}
+
 struct strbuff_t* charArray(int fd, struct  strbuff_t* sb) {
     char ch;
     sb->data = malloc(sizeof(char));
@@ -132,28 +215,37 @@ struct strbuff_t* charArray(int fd, struct  strbuff_t* sb) {
     return sb;
 }
 
-void destroyList(struct llnode* list) {
-    struct llnode* curr = list;
-    struct llnode* next = NULL;
+void destroyList(struct fileRepository* fileStruct) {
 
+    struct fileRepository* curr = fileStruct;
+    struct fileRepository* next = NULL;
     while (curr != NULL) {
-        next = curr->next;
-        free(curr->word);
+        struct llnode* c = curr->list;
+        struct llnode* n = NULL;
+
+        while (c != NULL) {
+            n = c->next;
+            free(c->word);
+            free(c);
+            c = n;
+        }
+        free(curr->fileName);
+        next = curr->nextFile;
         free(curr);
         curr = next;
     }
-
-    list = NULL;
 }
 
-int main(int argc, char** argv) {
+
+
+struct llnode* analyzeText(int fd_in){
     int i = 0;
-    int fd_in;
-    fd_in = open(argv[1], O_RDONLY);
+
     struct strbuff_t sb;
     struct llnode* list = NULL;
     charArray(fd_in, &sb);
     printf("-------\nInput Text:\n%s\n-------\n", sb.data);
+
     struct strbuff_t temp;
     temp.data = malloc(sizeof(char));
     temp.data[0] = '\0';
@@ -164,7 +256,7 @@ int main(int argc, char** argv) {
 
         if ((!ispunct(sb.data[i]) && !isspace(sb.data[i]) && !(sb.data[i] == '\0')) || sb.data[i] == '-') { // if it's not a whitespace, punct, or is a hypen, enter this
             sb_append(&temp, sb.data[i]);
-        } else if (isspace(sb.data[i]) || sb.data[i] == '\0') {
+        } else if ((isspace(sb.data[i]) || sb.data[i] == '\0') && temp.data[0] != '\0') {
             list = insertNode(list, &temp);
             free(temp.data); // reset the temp stuct values
             temp.data = malloc(sizeof(char));
@@ -176,9 +268,66 @@ int main(int argc, char** argv) {
         i++;
     }
 
-    printList(list);
+    updateFrequencies(&list);
     sb_destroy(temp.data);
     sb_destroy(sb.data);
-    destroyList(list);
+
+    return list;
+}
+
+
+
+int main(int argc, char** argv) {
+
+    int d = 1; // four arguments with default values
+    int f = 1;
+    int a = 1;
+    char* suffix = malloc(sizeof(char) *  5);
+    strcpy(suffix, ".txt");
+
+    struct fileRepository* fileList = NULL;
+
+    for (int i = 1; i < argc; i++) {
+        
+        char* temp;
+
+        if (argv[i][0] == '-') { // if its a optional argument
+            if (argv[i][1] == 'd') {
+                temp = malloc(sizeof(char) * strlen(argv[i]));
+                strncpy(temp, &argv[i][2], strlen(argv[i])-1);
+                d = atoi(temp);
+                free(temp);
+            } else if (argv[i][1] == 'f') {
+                temp = malloc(sizeof(char) * strlen(argv[i]));
+                strncpy(temp, &argv[i][2], strlen(argv[i])-1);
+                f = atoi(temp);
+                free(temp);
+            } else if (argv[i][1] == 'a') {
+                temp = malloc(sizeof(char) * strlen(argv[i]));
+                strncpy(temp, &argv[i][2], strlen(argv[i])-1);
+                a = atoi(temp);
+                free(temp);
+            } else if (argv[i][1] == 's') {     
+                suffix = realloc(suffix, sizeof(char) * strlen(argv[i]));
+                strncpy(suffix, &argv[i][2], strlen(argv[i])-1);
+            } else {
+                continue;
+            }
+
+        } else { 
+            int fd_in = open(argv[i], O_RDONLY);
+            struct llnode* tempList = analyzeText(fd_in);
+            // printList(tempList);
+
+            // printf("File: %s\n", argv[i]);
+            
+            fileList = insertWFD(fileList, tempList, argv[i]);
+        }
+    }
+    printWFDList(fileList);
+    printf("-d: %d | -f: %d | -a: %d | -s: %s\n", d, f, a, suffix);
+    
+    free(suffix);
+    destroyList(fileList);
     return EXIT_SUCCESS;
 }
